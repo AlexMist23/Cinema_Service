91a99d892b03f600ad32234f373ad082bcd0f112
Alex <aleksander.misterkiewicz@outlook.com>
2022-11-28 20:53:52 +0100

    First day - Landing page, html, css

--- Cinema_Service/settings.py
+++ Cinema_Service/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Cinema_Service.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'cinema_app/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
