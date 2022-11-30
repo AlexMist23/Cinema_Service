63f2713df70f6ba7eba2af8d91393fdb5dd79b71
Alex <aleksander.misterkiewicz@outlook.com>
2022-11-30 19:07:55 +0100

    Day 3 - making forms

--- cinema_app/views.py
+++ cinema_app/views.py
@@ -1,7 +1,15 @@
+import django
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
-from django.views import View, generic
+from django.views import View, generic, decorators
+from django.views.decorators.csrf import csrf_exempt
 from django.urls import reverse_lazy
+
+
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
 
 
 class LandingPage(View):
@@ -9,7 +17,11 @@ class LandingPage(View):
         return render(request, 'landing_page.html')
 
 
-class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy("login")
-    template_name = "registration/signup.html"
+class AddCinema(View):
+    def get(self, request):
+        return render(request, 'cinema_form.html')
+
+    @csrf_exempt
+    def post(self, request):
+        cnx = {}
+        return render(request, 'add_cinema', cnx)
